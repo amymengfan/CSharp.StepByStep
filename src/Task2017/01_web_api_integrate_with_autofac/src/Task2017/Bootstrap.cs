afbe4ef59bc4e3de87997c7ca933f2b241663f07
mengfan <mffeng@thoughtworks.com>
2017-08-08 00:03:20 +0800

    finish web api integrate with autofac

---
+++ src/Task2017/01_web_api_integrate_with_autofac/src/Task2017/Bootstrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Task2017.Services;
+
+namespace Task2017
+{
+    public class Bootstrap
+    {
+        public static void Init(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<TaskService>();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(builder.Build());
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+        }
+    }
+}
